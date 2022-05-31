24a0d5142d72e69390f762e7f8119e18c9cfce0c
CoderWDD <76061542+CoderWDD@users.noreply.github.com>
2022-05-31 03:35:15 +0000

    Deploying to gh-pages from @ CoderWDD/CoderWDD.github.io@6435cef45e6533832a3fdc28d8896a36604ff9d6 🚀

--- assets/index.html.140c0019.js
+++
@@ -1,8 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{d as i}from"./app.22176dd6.js";const n={},a=i(`<h1 id="c-\u57FA\u7840\u5165\u95E8" tabindex="-1"><a class="header-anchor" href="#c-\u57FA\u7840\u5165\u95E8" aria-hidden="true">#</a> C++\u57FA\u7840\u5165\u95E8</h1><div class="language-c++ ext-c++ line-numbers-mode"><pre class="language-c++"><code>class myName{
-    public:
-    	int a;
-};
-virtual int myTest(){
-    return 10
-}
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2);function d(s,r){return a}var t=e(n,[["render",d],["__file","index.html.vue"]]);export{t as default};
